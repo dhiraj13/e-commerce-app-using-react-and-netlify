fe8597bc8ce6ae63000e204f5cf220121dbe20dd
Dhiraj Khapangi <dkhapangi@outlook.com>
2019-08-24 21:45:01 +0545

    working on single item remove functionality

--- src/context.js
+++ src/context.js
@@ -68,14 +68,32 @@ class ProductProvider extends Component {
             return { modalOpen: false }
         })
     };
-    increment = (id) => {
+    increment = id => {
         console.log('this is incremental method');
     };
-    decrement = (id) => {
+    decrement = id => {
         console.log('this is decremental method');
     };
-    removeItem = (id) => {
-        console.log('item removed');
+    removeItem = id => {
+        let tempProducts = [...this.state.products];
+        let tempCart = [...this.state.cart];
+
+        tempCart = tempCart.filter(item => item.id !== id);
+
+        const index = tempProducts.indexOf(this.getItem(id));
+        let removedProduct = tempProducts[index];
+        removedProduct.inCart = false;
+        removedProduct.count = 0;
+        removedProduct.total = 0;
+
+        this.setState(() => {
+            return {
+                cart: [...tempCart],
+                products: [...tempProducts]
+            }
+        }, () => {
+            this.addTotals();
+        })
     };
     clearCart = () => {
         this.setState(
